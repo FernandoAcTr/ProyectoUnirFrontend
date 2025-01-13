f112fe81ebc8de247f0d034f54ea04e3ef79a54d
Fernando Acosta <fernandodeveloper99@gmail.com>
2025-01-12 19:15:32 -0600

    feat: agregar menú desplegable en la Navbar con opciones de ayuda

--- src/layout/guest/navbar.tsx
+++ src/layout/guest/navbar.tsx
@@ -42,11 +42,50 @@ export const Navbar = () => {
             Contacto
           </Link>
         </li>
-        <li className='opacity-40'>
-          <Link to='#' className='text-white'>
+        <li className='group relative'>
+          <button
+            className={`text-white flex items-center gap-2 ${
+              isMenuActive('/padecimientos') || isMenuActive('/forma-cara') || isMenuActive('/faqs')
+                ? 'opacity-100'
+                : 'opacity-40'
+            }`}
+          >
             Ayuda
-          </Link>
+            <i className='fa-solid fa-chevron-down'></i>
+          </button>
+          <div
+            id='dropdownNavbar'
+            className='z-10 hidden group-hover:block bg-white divide-y divide-gray-100 rounded-md shadow w-44 absolute right-0'
+          >
+            <ul className='py-2 text-sm text-gray-700 dark:text-gray-400' aria-labelledby='dropdownLargeButton'>
+              <li>
+                <Link
+                  to='/padecimientos'
+                  className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-accent-400 dark:hover:text-white'
+                >
+                  Padecimientos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to='/forma-cara'
+                  className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-accent-400 dark:hover:text-white'
+                >
+                  Forma de Cara
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to='/faqs'
+                  className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-accent-400 dark:hover:text-white'
+                >
+                  Preguntas Frecuentes
+                </Link>
+              </li>
+            </ul>
+          </div>
         </li>
+
         <li className='opacity-40'>
           <i className='fa-solid fa-cart-plus text-white'></i>
         </li>

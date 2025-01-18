e96d3fb3ea3a7a21b20b102b85651167a6b79681
Fernando Acosta <fernandodeveloper99@gmail.com>
2025-01-18 14:31:18 -0600

    boton call to action responsive

--- src/pages/home/call_to_action.tsx
+++ src/pages/home/call_to_action.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 const CallToAction = () => {
   return (
     <section className='mt-12 h-52 flex justify-center items-center'>
-      <Link to='#' className='border-2 border-black py-8 px-28 text-3xl font-light rounded-full hover:bg-accent-400 hover:text-black transition-all'>
+      <Link to='#' className='border-2 border-black py-5 lg:py-8 px-10 lg:px-28 text-3xl font-light rounded-full hover:bg-accent-400 hover:text-black transition-all'>
         Contáctanos
       </Link>
     </section>
